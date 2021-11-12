b5bb7167f83930264212f8dc89a3b605db5cc08f
ErAnirudha <93269088+ErAnirudha@users.noreply.github.com>
2021-11-12 20:07:56 +0530

    calculator program in c++
    calculator program in c++ include add, sub, mul, div and modulus...

---
+++ calculator.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout<<"Hello Welcome to world of Calculator:"<<endl<<"\n";
+    cout<<"Created By: Anirudha Kulkarni:"<<endl<<"\n";
+    int a,b;
+    cout<<"Enter two values A and B:"<<endl<<"\n";
+    cout<<"Value of A:\t";
+    cin>>a;
+    cout<<"Value of B:\t";
+    cin>>b;
+    cout<<"\n\tAddition:\t"<<a+b<<endl<<"\n";
+    cout<<"\tsubstraction:\t"<<a-b<<endl<<"\n";
+    cout<<"\tMultiplication:\t"<<a*b<<endl<<"\n";
+    cout<<"\tDivision:\t"<<a/b<<endl<<"\n";
+    cout<<"\tModulus:\t"<<a%b<<endl<<"\n";
+return 0;
+}
