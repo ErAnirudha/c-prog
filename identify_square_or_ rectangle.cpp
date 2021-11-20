92257dfd3b8e239195e25ca74d5da360354d29d9
ErAnirudha <93269088+ErAnirudha@users.noreply.github.com>
2021-11-20 15:14:42 +0530

    identify square or not
    simply just identify square or not

---
+++ identify_square_or_ rectangle.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+int length,breadth;
+cout<<"enter length"<<endl;
+cin>>length;
+cout<<"enter breadth"<<endl;
+cin>>breadth;
+if(length==breadth){
+
+cout<<"its square"<<endl;
+}
+else
+cout<<"its rectangle"<<endl;
+return 0;
+}
