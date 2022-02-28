09a2a92ee895217c01e04f6465a4c48324879824
ErAnirudha <93269088+ErAnirudha@users.noreply.github.com>
2022-02-28 08:35:09 +0530

    basic math fuctions
    some basic math functions in c++ we use directly

---
+++ cmath_functions.cpp
@@ -0,0 +1,29 @@
+// some math operations using <cmath>
+#include<iostream>
+#include<cmath>
+//#include<math.h> //we can use <cmath> or <math.h>   
+using namespace std;
+int main(){
+//	cout<<"square="<<square(4)<<"\n";
+	cout<<"power of="<<pow(3,3)<<"\n";
+	cout<<"square_root="<<sqrt(64.5)<<"\n";
+	cout<<"returns cuberoot="<<cbrt(64)<<"\n";
+	cout<<"absolute value="<<abs(-10)<<"\n";
+	cout<<"ceil value="<<ceil(3.578)<<"\n";
+	cout<<"floor value="<<floor(3.578)<<"\n";
+	cout<<"in form of e to pow x="<<exp(3000)<<"\n";
+	cout<<"modulus value="<<fmod(3.4,4)<<"\n";
+	cout<<"minimum value="<<min(15,255)<<"\n";
+	cout<<"maximum value="<<max(355,13)<<"\n";
+	cout<<"return least floating point value="<<fmin(3.4,3.3)<<"\n";
+	cout<<"return greater floating point value="<<fmax(3.4,3.3)<<"\n";
+	cout<<"round number="<<round(3.6)<<"\n";
+	cout<<"round number="<<round(3.4)<<"\n";
+	cout<<"round number="<<round(3.5)<<"\n";
+	cout<<"to find log number="<<log(2)<<"\n";
+	cout<<"trignometry tangent value tan="<<tan(5)<<"\n";
+	cout<<"trignometry sin value sin="<<sin(5)<<"\n";
+	cout<<"trignometry cosine value cos="<<cos(5)<<"\n";
+	cout<<"trignometry cotangent value cot="<<atan(5)<<"\n";
+	return 0;
+}
